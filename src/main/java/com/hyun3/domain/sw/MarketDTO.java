a9267530f9c2437ae06c43bef07f6357a4ce6752
user <user@DESKTOP-ONKB0HH>
2024-12-06 14:36:58 +0900

    commit

---
+++ src/main/java/com/hyun3/domain/sw/MarketDTO.java
@@ -0,0 +1,11 @@
+package com.hyun3.domain.sw;
+
+public class MarketDTO {
+	private int marketNum;
+	private String title;
+	private String content;
+	private String ca_date;
+	private String fileName;
+	private int mb_num;
+	private int ct_num;
+}
